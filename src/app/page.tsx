3c9d1b7d43eb84340852ecdb915a80f637e4eeb3
KcPele <fidekg123@gmail.com>
2023-08-11 23:07:45 +0100

    navbar done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,21 +3,15 @@ import Hero from "@/components/Home/Hero";
 import Navbar from "@/components/Home/Navbar";
 import What from "@/components/Home/What";
 
-
-
 export default function HomePage() {
   return (
     <>
-    <div
-      className=" bg-cover bg-center h-screen w-full bg px-[5%]"
-    >
-      <Navbar />
-      <Hero />
-      
-    </div>
-    <What />
-    <Footer />
+      <div className=" bg-cover bg-center h-screen w-full bg px-[5%]">
+        <Navbar />
+        <Hero />
+      </div>
+      <What />
+      <Footer />
     </>
-
   );
 }
